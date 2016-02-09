c9e90d92ba051861dcc8ca20279626cbc6825b87
“Jeremiah <jeremiah.reichardt@byhook.com>
2016-02-08 18:02:18 -0800

    tweeqable params are defined in a object

--- app/components/Background.js
+++ app/components/Background.js
@@ -6,31 +6,36 @@ var tweeq = require('../tweeq/index');
 
 function Background() {
 
-  this.background = undefined;
-  this.scale = [0.25, 0.25];
-  this.aspect = 1;
-  this.color1 = [1, 1, 1];
-  this.color2 = [0, 0, 0];
-  this.smoothing = [0.10, 0.5];
-  this.noiseAlpha = 0;
-  this.coloredNoise = true;
-  this.offset = [-0.05, -0.15];
+  this.tweeqable = {
+    scale1:{ default: 0.25, min: -1, max: 1 },
+    aspect:{ default: 1, min: 0, max: 1 },
+    scale2:{ default: 0.25, min: -1, max: 1 },
+    red1: {  default: 1, min: 0, max: 1 },
+    green1: { default: 1, min: 0, max: 1 },
+    blue1: { default: 1, min: 0, max: 1 },
+    red2: {  default: 0, min: 0, max: 1 },
+    green2: { default: 0, min: 0, max: 1 },
+    blue2: { default: 0, min: 0, max: 1 },
+    smoothing1: { default: 0.10, min: 0, max: 1 },
+    smoothing2: { default: 0.5, min: 0, max: 1 },
+    noiseAlpha: { default: 0, min: 0, max: 1 },
+    offset1: { default: -0.05, min: -1, max: 1 },
+    offset2: { default: -0.15, min: -1, max: 1 }
+  };
 
   var controls = tweeq.container();
   controls.mount(document.querySelector('#controls'));
-  controls.add('Scale 1', 0.5, { min: 0, max: 1 }).changed( this.tweeqScale1.bind( this ));
-  controls.add('Scale 2', 0.5, { min: 0, max: 1 }).changed( this.tweeqScale2.bind( this ));
-  controls.add('Aspect', 0.5, { min: 0, max: 1 }).changed( this.tweeqAspect.bind( this ));
-  controls.add('Red 1', 0.5, { min: 0, max: 1 }).changed( this.tweeqColor1R.bind( this ));
-  controls.add('Green 1', 0.5, { min: 0, max: 1 }).changed( this.tweeqColor1G.bind( this ));
-  controls.add('Blue 1', 0.5, { min: 0, max: 1 }).changed( this.tweeqColor1B.bind( this ));
-  controls.add('Red 2', 0.5, { min: 0, max: 1 }).changed( this.tweeqColor2R.bind( this ));
-  controls.add('Green 2', 0.5, { min: 0, max: 1 }).changed( this.tweeqColor2G.bind( this ));
-  controls.add('Blue 2', 0.5, { min: 0, max: 1 }).changed( this.tweeqColor2B.bind( this ));
-  controls.add('Smoothing 1', 0.5, { min: 0, max: 1 }).changed( this.tweeqSmoothing1.bind( this ));
-  controls.add('Smoothing 2', 0.5, { min: 0, max: 1 }).changed( this.tweeqSmoothing2.bind( this ));
-  controls.add('Offset 1', 0.5, { min: -1, max: 1 }).changed( this.tweeqOffset1.bind( this ));
-  controls.add('Offset 2', 0.5, { min: -1, max: 1 }).changed( this.tweeqOffset2.bind( this ));
+
+  for ( var key in this.tweeqable ) {
+    var val = this.tweeqable[key];
+    val.value = val.default;
+    var obj = {};
+    obj.func = function( value ){
+      this.val.value = value;
+    };
+    obj.val = val;
+    controls.add( key, val.default, { min: val.min, max: val.max }).changed( obj.func.bind( obj ) );
+  }
 
   shell.on('gl-init', this.init.bind( this ));
   shell.on('gl-render', this.render.bind( this ));
@@ -42,75 +47,16 @@ Background.prototype.init = function() {
 
 Background.prototype.render = function() {
   this.background.style({
-    // xy scale
-    scale: this.scale,
-    // aspect ratio for vignette
-    aspect: this.aspect,
-    // radial gradient colors A->B
-    color1: this.color1,
-    color2: this.color2,
-    // smoothstep low/high input
-    smoothing: this.smoothing,
-    // % opacity of noise grain (0 -> disabled)
-    noiseAlpha: this.noiseAlpha,
-    // whether or not the noise is monochromatic
+    scale: [this.tweeqable.scale1.value, this.tweeqable.scale2.value],
+    aspect: this.tweeqable.aspect.value,
+    color1: [this.tweeqable.red1.value, this.tweeqable.green1.value, this.tweeqable.blue1.value],
+    color2: [this.tweeqable.red2.value, this.tweeqable.green2.value, this.tweeqable.blue2.value],
+    smoothing: [this.tweeqable.smoothing1.value, this.tweeqable.smoothing2.value],
+    noiseAlpha: this.tweeqable.noiseAlpha.value,
     coloredNoise: true,
-    // offset the vignette
-    offset: this.offset
+    offset: [this.tweeqable.offset1.value, this.tweeqable.offset2.value]
   });
   this.background.draw();
-};
-
-Background.prototype.tweeqScale1 = function( value ) {
-  this.scale[0] = value;
-};
-
-Background.prototype.tweeqScale2 = function( value ) {
-  this.scale[1] = value;
-};
-
-Background.prototype.tweeqAspect = function( value ) {
-  this.aspect = value;
-};
-
-Background.prototype.tweeqColor1R = function( value ) {
-  this.color1[0] = value;
-};
-
-Background.prototype.tweeqColor1G = function( value ) {
-  this.color1[1] = value;
-};
-
-Background.prototype.tweeqColor1B = function( value ) {
-  this.color1[2] = value;
-};
-
-Background.prototype.tweeqColor2R = function( value ) {
-  this.color2[0] = value;
-};
-
-Background.prototype.tweeqColor2G = function( value ) {
-  this.color2[1] = value;
-};
-
-Background.prototype.tweeqColor2B = function( value ) {
-  this.color2[2] = value;
-};
-
-Background.prototype.tweeqSmoothing1 = function( value ) {
-  this.smoothing[0] = value;
-};
-
-Background.prototype.tweeqSmoothing2 = function( value ) {
-  this.smoothing[1] = value;
-};
-
-Background.prototype.tweeqOffset1 = function( value ) {
-  this.offset[0] = value;
-};
-
-Background.prototype.tweeqOffset2 = function( value ) {
-  this.offset[1] = value;
 };
 
 module.exports = Background;
